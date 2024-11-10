f15930bda0bb9f737d2d8aed0dd5d9a77db5c5bd
Bambang Heri Setiawan <setiawan.heri.bambang@gmail.com>
2024-11-10 11:02:04 +0700

    executable installer for core component filament

---
+++ .filamen_core.sh
@@ -0,0 +1,12 @@
+ #!/bin/bash
+composer require filament/filament:"^3.2" -W &&
+php artisan filament:install --panels &&
+php artisan vendor:publish --tag=filament-config &&
+php artisan vendor:publish --tag=filament-actions-translations &&
+php artisan vendor:publish --tag=filament-forms-translations &&
+php artisan vendor:publish --tag=filament-infolists-translations &&
+php artisan vendor:publish --tag=filament-notifications-translations &&
+php artisan vendor:publish --tag=filament-tables-translations &&
+php artisan vendor:publish --tag=filament-translations &&
+php artisan migrate &&
+php artisan make:filament-user
